5a925bcff46ca0d378fa75298ec60856d241d59f
Kroustouris Stavros <kroustou@gmail.com>
2013-01-29 11:40:58 +0200

    add requirejs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-$(function(){
+/*global require*/
+require.config({
+    paths: {
+        // jquery always need a path config if it's not on the root directory
+        'jquery': 'lib/jquery'
+    }
+});
 
-	var body = $('body');
-	$(document).on("scroll", function(){
-		var scrollTop = $(document).scrollTop();
-		if (scrollTop > "160"){
-			body.addClass("fixed");
-		} else {
-			body.removeClass("fixed");
-		}
-	});
-}());
+require(['jquery'], function($) {
+    'use strict';
+    var body = $('body');
+    $(document).on('scroll', function(){
+        var scrollTop = $(document).scrollTop();
+        if (scrollTop > '160'){
+            body.addClass('fixed');
+        } else {
+            body.removeClass('fixed');
+        }
+    });
+});
